c21f081154ed9e3adfbb4db0a37efba372826f80
qqdasb <congminda188@outlook.com>
2025-03-22 21:38:56 +0800

    Init

---
+++ packages/termux-wsi-layer/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://termux.dev
+TERMUX_PKG_DESCRIPTION="Termux's ICD/WSI wrapper for using with proprietary Android drivers"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.0.1"
+TERMUX_PKG_SKIP_SRC_EXTRACT=true
+TERMUX_PKG_BUILD_DEPENDS="libglvnd, libxcb, libx11"
+TERMUX_PKG_NO_STRIP=false
+
+TERMUX_PKG_SRCDIR="$TERMUX_PREFIX/opt/termux-wsi-layer/src"
+
+termux_step_pre_configure() {
+	export CFLAGS="${CFLAGS/-Oz/-O2} -flto"
+	mkdir -p "$TERMUX_PREFIX/opt/termux-wsi-layer/src"
+	cp -r "${TERMUX_PKG_BUILDER_DIR}/"{*.c,*.h,*.json,CMakeLists.txt} "$TERMUX_PREFIX/opt/termux-wsi-layer/src"
+}
